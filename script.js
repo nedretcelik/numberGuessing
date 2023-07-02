190ad3b752bf048d441712768137a046bda8294f
Nedret Çelik <nedretcelik97@gmail.com>
2023-07-02 12:30:02 -0400

    JavaScript functions to complate the  guessing game.

--- script.js
+++ script.js
@@ -2,5 +2,34 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-// Write your code below:
 
+const generateTarget = () => {
+    return Math.floor(Math.random()*9);
+
+
+} 
+
+const compareGuesses = (humanG, computerG, targetNum) => {
+    const humanDifference = Math.abs(targetNum -humanG);
+    const computerDifference = Math.abs(targetNum -computerG);
+
+    if(humanDifference <= computerDifference) {
+        return true;
+    }else {
+        return false;
+    }
+}
+
+const updateScore = winner => {
+    if(winner === "human") {
+        humanScore++;
+    } else if ("computer"){
+        computerScore++;
+    }
+
+}
+
+const advanceRound =() => {
+    currentRoundNumber ++;
+
+}
